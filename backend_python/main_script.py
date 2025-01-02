69866af12c9d28f9eba38d78a22aa716c636f5d6
tuokcalb <paringupta4871@gmail.com>
2025-01-02 11:13:06 -0500

    try-catch block for query_database

--- backend_python/main_script.py
+++ backend_python/main_script.py
@@ -7,13 +7,18 @@ import os
 # in form of dict; Also dumps table into execl file
 def query_database(query):
     client = bigquery.Client(project='ethereal-orb-445410-r4')
-    query_job = client.query(query)  
+    try:
+        query_job = client.query(query)  
 
-    results_df = query_job.result().to_dataframe()
-    results_df.to_excel("output.xlsx") 
+        results_df = query_job.result().to_dataframe()
+        results_df.to_excel("output.xlsx") 
 
-    return results_df.to_dict(orient='records')
+        return { 'success': True, 'message': results_df.to_dict(orient='records') }
+   
+    except Exception as e:
 
+        return { 'success': False, 'message': str(e) }
+    
 # Provides a list of all public databases hosted on big 
 # query; Slow needs to be streamed ???how though
 def show_tables():
